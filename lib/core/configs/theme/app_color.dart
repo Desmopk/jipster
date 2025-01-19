59438e46ef4df0d249d34415bc1d13995db47086
Desmopk <pranav.kumar2022@vitstudent.ac.in>
2025-01-19 23:06:34 +0530

    theme

---
+++ lib/core/configs/theme/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors
+{
+  static const primary= Color.fromRGBO(200, 160, 255, 1.0);
+  static const lightBackground= Color.fromRGBO(242, 242, 242, 1.0);
+  static const darkBackground= Color.fromRGBO(13, 12, 12, 1.0);
+
+}
